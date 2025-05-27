5a81add4c689413732c7faa7a44f2333364ae653
shoile azeez <shoabdulazeez@gmail.com>
2025-05-27 05:51:51 +0100

    handling loop in javascript

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -3,3 +3,6 @@ const words = ["C is fun", "Python is cool", "JavaScript is amazing"]
 for (let i = 0; i < words.length; i++){
     console.log(words[i])
 }
+// for (let i in words){
+//     console.log(words[i]);
+// }
