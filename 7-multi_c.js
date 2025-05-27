4f7ee4f3c0f19104429f6db1fd3957834dcc6950
shoile azeez <shoabdulazeez@gmail.com>
2025-05-27 05:54:12 +0100

    changing number into integer using parseint

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,6 @@
 const x = Number(process.argv[2]);
 
-if (isNaN(x) || x < 0 || !Number.isInteger(x)) {
+if (isNaN(x) || !Number.isInteger(x)) {
     console.log("Missing number of occurrences");
 } else {
     for (let i = 0; i < x; i++) {
